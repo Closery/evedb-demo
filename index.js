9f216aab72c824773a5dc8a3d26db26d76035fc2
Closery <yunus@clofro.com>
2021-06-20 05:56:09 +0300

    port fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
