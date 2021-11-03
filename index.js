022713134fc777211a4459a8772a7a4527c524f6
Yunus Emre Uzun <yunus@clofro.com>
2021-11-04 02:14:38 +0300

    IMDB Detail added

--- index.js
+++ index.js
@@ -5,22 +5,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { GET_MovieFromIMDB, MyAnimeListDetail, MyAnimeListSearch, IMDBSearch } = require('./functions');
+const { MyAnimeListDetail, MyAnimeListSearch, IMDBSearch, IMDBDetail } = require('./functions');
 
 app.get('/', (req, res) => {
 	res.send('Welcome to MyList API');
-	// const { movie_url, anime_url } = req.query;
-	// if (movie_url) {
-	// 	GET_MovieFromIMDB(movie_url).then((data) => {
-	// 		res.send(data.title);
-	// 	});
-	// } else if (anime_url) {
-	// 	MyAnimeListDetail(anime_url).then((data) => {
-	// 		res.send(data.title);
-	// 	});
-	// } else {
-	// 	res.send({ error: 'Bir sorun oluştu' });
-	// }
 });
 
 app.get('/api/imdb/search', (req, res) => {
@@ -34,6 +22,32 @@ app.get('/api/imdb/search', (req, res) => {
 		res.statusCode = 400;
 		res.send({ code: 400, message: 'You need to use "s" parameter to search. Example: ?s=Hunter x Hunter' });
 	}
+});
+
+app.get('/api/imdb/search', (req, res) => {
+	const { s } = req.query;
+
+	if (s) {
+		IMDBSearch(s).then((data) => {
+			res.send(data);
+		});
+	} else {
+		res.statusCode = 400;
+		res.send({ code: 400, message: 'You need to use "s" parameter to search. Example: ?s=Hunter x Hunter' });
+	}
+});
+
+app.get('/api/imdb/detail/:type/:id', (req, res) => {
+	const { id, type } = req.params;
+
+	IMDBDetail(id, type).then((data) => {
+		if (data.status) {
+			res.send(data);
+		} else {
+			res.statusCode = data.statusCode;
+			res.send(data);
+		}
+	});
 });
 
 app.get('/api/myanimelist/search', (req, res) => {
